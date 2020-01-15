4a5e0d51ee7c71d5b12e97dc75bc1591e9fb7ede
kittisak.tos <ruzerie@gmail.com>
2020-01-15 23:10:10 +0700

    create product controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAPI_example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public Product Get()
+        {
+            return new Product
+            {
+                Id = 111,
+                Name = "iAmYacul",
+                Price = 9999
+            };
+        }
+
+        [HttpPost]
+        public Product CreateNewProduct([FromBody]Product data)
+        {
+            //insert new product to DB
+            //return "Create " + data.Name + " Done";
+
+            data.Id = 99;
+            return data;
+        }
+
+        [HttpPut]
+        public string ChangeProductName([FromBody]Product data)
+        {
+
+            //update product name in DB
+
+            return "Update Product Name " + data.Name + " Success";
+        }
+    }
+}
